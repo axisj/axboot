2ed7f4987db98261dcb360a7ad9c88d860bb3988
tom <tom@axisj.com>
2024-07-11 16:42:44 +0900

    reinit project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended","prettier"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }], // for windows
+    "no-empty-pattern": 0,
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react-hooks/exhaustive-deps": "error",
+    "@typescript-eslint/no-empty-function": 1,
+    "@typescript-eslint/no-empty-interface": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/no-unused-vars": [
+      1,
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
